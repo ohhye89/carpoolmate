ecb30fd3d60ca31bb66d99ee36d9b7d8a130de16
jaehoonlee1010 <jaehoonlee1010@gmail.com>
2012-12-30 01:36:34 +0900

    TabHost를 Activity로 처리하였다.

--- gen/com/alphastudio/carpoolmate/R.java
+++ gen/com/alphastudio/carpoolmate/R.java
@@ -20,30 +20,31 @@ public final class R {
         public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int RelativeLayout1=0x7f080001;
-        public static final int login_btn_find=0x7f080007;
-        public static final int login_btn_login=0x7f080006;
-        public static final int login_edittxt_id=0x7f080003;
-        public static final int login_edittxt_password=0x7f080005;
-        public static final int login_image_logo=0x7f080000;
-        public static final int login_txt_id=0x7f080002;
-        public static final int login_txt_password=0x7f080004;
-        public static final int main_checkin_btn_send=0x7f08000b;
-        public static final int main_checkin_txtview_name=0x7f08000a;
-        public static final int main_chekckin_txtview_date=0x7f080009;
-        public static final int main_history_btn_detail=0x7f080010;
-        public static final int main_history_prg_loading=0x7f080011;
-        public static final int main_history_txtview_count=0x7f08000e;
-        public static final int main_history_txtview_name=0x7f08000d;
-        public static final int main_history_txtview_total=0x7f08000f;
-        public static final int main_tap_checkin=0x7f080008;
-        public static final int main_tap_history=0x7f08000c;
-        public static final int menu_settings=0x7f080012;
+        public static final int RelativeLayout1=0x7f080009;
+        public static final int checkin_btn_send=0x7f080002;
+        public static final int checkin_txtview_name=0x7f080001;
+        public static final int chekckin_txtview_date=0x7f080000;
+        public static final int history_btn_detail=0x7f080006;
+        public static final int history_prg_loading=0x7f080007;
+        public static final int history_txtview_count=0x7f080004;
+        public static final int history_txtview_name=0x7f080003;
+        public static final int history_txtview_total=0x7f080005;
+        public static final int login_btn_find=0x7f08000f;
+        public static final int login_btn_login=0x7f08000e;
+        public static final int login_edittxt_id=0x7f08000b;
+        public static final int login_edittxt_password=0x7f08000d;
+        public static final int login_image_logo=0x7f080008;
+        public static final int login_txt_id=0x7f08000a;
+        public static final int login_txt_password=0x7f08000c;
+        public static final int menu_settings=0x7f080011;
+        public static final int tabhost=0x7f080010;
     }
     public static final class layout {
-        public static final int activity_loading=0x7f030000;
-        public static final int activity_login=0x7f030001;
-        public static final int activity_main=0x7f030002;
+        public static final int activity_checkin=0x7f030000;
+        public static final int activity_history=0x7f030001;
+        public static final int activity_loading=0x7f030002;
+        public static final int activity_login=0x7f030003;
+        public static final int activity_main=0x7f030004;
     }
     public static final class menu {
         public static final int activity_loading=0x7f070000;
@@ -51,7 +52,11 @@ public final class R {
     public static final class string {
         public static final int LoadingSuccess=0x7f05000e;
         public static final int app_name=0x7f050000;
+        public static final int checkin_btn_send=0x7f05000a;
         public static final int hello_world=0x7f050001;
+        public static final int history_btn_detail=0x7f05000d;
+        public static final int history_txtview_count=0x7f05000c;
+        public static final int history_txtview_name=0x7f05000b;
         public static final int login_btn_find=0x7f050008;
         public static final int login_btn_login=0x7f050007;
         public static final int login_edittxt_id=0x7f050004;
@@ -59,10 +64,6 @@ public final class R {
         public static final int login_image_logo=0x7f050009;
         public static final int login_txt_id=0x7f050003;
         public static final int login_txt_password=0x7f050005;
-        public static final int main_checkin_btn_send=0x7f05000a;
-        public static final int main_history_btn_detail=0x7f05000d;
-        public static final int main_history_txtview_count=0x7f05000c;
-        public static final int main_history_txtview_name=0x7f05000b;
         public static final int menu_settings=0x7f050002;
     }
     public static final class style {
--- src/com/alphastudio/carpoolmate/MainActivity.java
+++ src/com/alphastudio/carpoolmate/MainActivity.java
@@ -6,6 +6,7 @@ import java.net.URL;
 import java.util.Calendar;
 import java.util.List;
 
+import android.app.ActivityGroup;
 import android.app.AlertDialog;
 import android.app.TabActivity;
 import android.content.DialogInterface;
@@ -32,273 +33,30 @@ import com.google.gdata.util.AuthenticationException;
 import com.google.gdata.util.ServiceException;
 
 @SuppressWarnings("deprecation")
-public class MainActivity extends TabActivity implements OnClickListener {
+public class MainActivity extends ActivityGroup implements OnClickListener {
 	private TabHost mTabHost;
-	private TextView mainCheckinEditTxtDate;
-	private TextView mainCheckinEditTxtName;
-	private TextView mainHistoryViewTxtName;
-	private TextView mainHistoryViewTxtCount;
-	private TextView mainHistoryViewTxtTotal;
-	private ProgressBar progress;
-	private Button sendBtn;
-	private Button detailBtn;
-	private Intent intent;
-
-	private String historyName;
-	private String historyCount;
-	private String historyTotal;
-	final static int GET_ID = 0;
-
-	String USERNAME = "ohej92@gmail.com";
-	String PASSWORD = "carpoolmate";
 	
-	String USERONE = "jaehoonlee1010";
-
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		mTabHost = getTabHost();
-		mTabHost.addTab(mTabHost.newTabSpec("tab1")
-				.setContent(R.id.main_tap_checkin)
-				.setIndicator("Check in"));
-		mTabHost.addTab(mTabHost.newTabSpec("tab2")
-				.setContent(R.id.main_tap_history)
-				.setIndicator("History"));
-
-		mainCheckinEditTxtDate = (TextView)findViewById(R.id.main_chekckin_txtview_date);
-		mainCheckinEditTxtName = (TextView)findViewById(R.id.main_checkin_txtview_name);
-		mainHistoryViewTxtName = (TextView)findViewById(R.id.main_history_txtview_name);
-		mainHistoryViewTxtCount = (TextView)findViewById(R.id.main_history_txtview_count);
-		mainHistoryViewTxtTotal = (TextView)findViewById(R.id.main_history_txtview_total);
-		progress = (ProgressBar)findViewById(R.id.main_history_prg_loading);
-
+		mTabHost = (TabHost)findViewById(R.id.tabhost);
 		
-		StringBuilder sb = new StringBuilder();
-		Calendar cal = Calendar.getInstance();
+		mTabHost.setup(getLocalActivityManager());
 		
-		sb.append("기분 좋은 하루 *^^* \n")
-		.append("오늘은 ")
-		.append(cal.get(Calendar.MONTH)+1)
-		.append("월 ")
-		.append(cal.get(Calendar.DATE))
-		.append("일 입니다.");
-		mainCheckinEditTxtDate.setText(sb.toString());
-
-		String name = "이재훈/오혜성";
-		StringBuilder sbName = new StringBuilder();
-		sbName.append("\""+name+"\"님 같이 타시렵니까?");
-		// mainCheckinEditTxtName.setText(sbName.toString());
-		
-		sendBtn = (Button)findViewById(R.id.main_checkin_btn_send);
-		detailBtn = (Button)findViewById(R.id.main_history_btn_detail);
-
-		sendBtn.setOnClickListener(this);
-		detailBtn.setOnClickListener(this);
-		
-		new AsyncTask<Void, Integer, Void>() {
-
-			@Override
-			protected void onPreExecute(){
-				//				publishProgress(0);
-			}
-
-			protected Void doInBackground(Void... params) {
-				try {
-					spreadSheetTest();
-				} catch (IOException e) {
-					e.printStackTrace();
-				} catch (ServiceException e) {
-					e.printStackTrace();
-				}
-
-				//				for(int i = 1; i <= 10 ; i++) {
-				//
-				//					try {
-				//						publishProgress(i);
-				//						Thread.sleep(100);
-				//					} catch (InterruptedException e) {
-				//						// TODO Auto-generated catch block
-				//						e.printStackTrace();
-				//					}
-				//				}
-
-				return null;
-			}
-
-			protected void onProgressUpdate(Integer... progressing){
-				//				progress.setProgress(progressing[0]);
-			}
-
-			protected void onPostExecute(Void result) {
-				// TODO Auto-generated method stub
-				super.onPostExecute(result);
-				Toast.makeText(MainActivity.this, R.string.LoadingSuccess, Toast.LENGTH_LONG).show();
-				progress.setVisibility(View.INVISIBLE);
-
-//				mainHistoryViewTxtName.setText(historyName.toString() + "님 !!");
-				mainHistoryViewTxtName.setText(USERNAME +"님!!");
-				mainHistoryViewTxtCount.setText(historyCount.toString() + "회 이용 하셨기 때문에,");
-				mainHistoryViewTxtTotal.setText("총 " + historyTotal.toString() + "원 납부요망");
-			}
-		}.execute();
+		mTabHost.addTab(mTabHost.newTabSpec("tab_checkin")
+		        .setIndicator("Check In")
+		        .setContent(new Intent(this, CheckInActivity.class)));
+		mTabHost.addTab(mTabHost.newTabSpec("tab_history")
+		        .setIndicator("History")
+		        .setContent(new Intent(this, HistoryActivity.class)));
 	}
 
 	@Override
 	public void onClick(View v) {
-		switch(v.getId())
-		{
-		case R.id.main_checkin_btn_send :
-			new AlertDialog.Builder(this)
-			.setTitle("전송")
-			.setMessage("전송하시겠습니까?")
-			.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-				@Override
-				public void onClick(DialogInterface dialog, int which) {
-					new SendToGooleDocs().execute();
-					Toast.makeText(MainActivity.this, "전송되었습니다.", 
-							Toast.LENGTH_SHORT).show();
-				}
-			})
-			.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-				@Override
-				public void onClick(DialogInterface dialog, int which) {
-					Toast.makeText(MainActivity.this, "취소되었습니다.", 
-							Toast.LENGTH_SHORT).show();						
-				}
-			}).create().show();
-			
-			break;
-			
-		case R.id.main_history_btn_detail :
-			intent = new Intent(Intent.ACTION_VIEW);
-			intent.setData(Uri.parse("https://docs.google.com/spreadsheet/ccc?key=0AhAUXFpCrNTedGYyS3FVV0ZhY1NYRGNPWVllNF9FbHc#gid=0"));
-			startActivity(intent);
-			break;
-		}
-	}
-	
-	private class SendToGooleDocs extends AsyncTask<Void, Void, Void>{
+		// TODO Auto-generated method stub
 		
-		@Override
-		protected Void doInBackground(Void... params) {
-			try {
-				send();
-			} catch (IOException e) {
-				e.printStackTrace();
-			} catch (ServiceException e) {
-				e.printStackTrace();
-			}
-			return null;
-		}
-		
-		@Override
-		protected void onPostExecute(Void result) {
-			super.onPostExecute(result);
-			mainCheckinEditTxtName.setText("됬냐?");
-		}
-
-		public void send() throws AuthenticationException, MalformedURLException, IOException, ServiceException {
-			SpreadsheetService service = new SpreadsheetService("MySpreadsheetIntegration-v1");
-			service.setUserCredentials(USERNAME, PASSWORD);
-			// 요청 URL정의. 바뀌지 않는다.
-			URL SPREADSHEET_FEED_URL = new URL("https://spreadsheets.google.com/feeds/spreadsheets/private/full");
-			// Make a request to the API and get all spreadsheets.
-			SpreadsheetFeed feed = service.getFeed(SPREADSHEET_FEED_URL, SpreadsheetFeed.class);
-			List<SpreadsheetEntry> spreadsheets = feed.getEntries();
-			if (spreadsheets.size() == 0) {      
-				// TODO: There were no spreadsheets, act accordingly.
-			}
-			
-			// 0번 인덱스 spreadsheet 출력
-			SpreadsheetEntry mySpreadsheet = spreadsheets.get(0);			
-			
-			// Make a request to the API to fetch information about all worksheets in the spreadsheet.
-			List<WorksheetEntry> worksheets = mySpreadsheet.getWorksheets();
-			
-			// 0번 인덱스 worksheet Title 출력
-			WorksheetEntry myWorksheet = worksheets.get(0);
-		   
-			URL listFeedUrl = myWorksheet.getListFeedUrl();
-		    ListFeed listFeed = service.getFeed(listFeedUrl, ListFeed.class);
-		    
-		    // 행
-		    ListEntry row = listFeed.getEntries().get(28);
-		    String currentCountString = row.getCustomElements().getValue(USERONE);
-		    int currentCountInt = Integer.parseInt(currentCountString);
-		    String setCount = String.valueOf(currentCountInt+1);
-    		row.getCustomElements().setValueLocal(USERONE, setCount);
-		    row.update();
-		}
-	}
-	
-	public void spreadSheetTest() throws IOException, ServiceException{
-		SpreadsheetService service = auth();
-		int i = 1;
-		int j = 1;
-
-		URL SPREADSHEET_FEED_URL = new URL(
-				"https://spreadsheets.google.com/feeds/spreadsheets/private/full");
-
-		// Make a request to the API and get all spreadsheets.
-		SpreadsheetFeed feed = service.getFeed(SPREADSHEET_FEED_URL,
-				SpreadsheetFeed.class);
-		List<SpreadsheetEntry> spreadsheets = feed.getEntries();
-
-		if (spreadsheets.size() == 0) {
-			// TODO: There were no spreadsheets, act accordingly.
-		}
-
-		// TODO: Choose a spreadsheet more intelligently based on your
-		// app's needs.
-		SpreadsheetEntry spreadsheet = spreadsheets.get(0);
-		System.out.println(spreadsheet.getTitle().getPlainText());
-
-		// Get the first worksheet of the first spreadsheet.
-		// TODO: Choose a worksheet more intelligently based on your
-		// app's needs.
-		WorksheetFeed worksheetFeed = service.getFeed(
-				spreadsheet.getWorksheetFeedUrl(), WorksheetFeed.class);
-		List<WorksheetEntry> worksheets = worksheetFeed.getEntries();
-		WorksheetEntry worksheet = worksheets.get(0);
-
-		// Fetch the list feed of the worksheet.
-		URL listFeedUrl = worksheet.getListFeedUrl();
-		ListFeed listFeed = service.getFeed(listFeedUrl, ListFeed.class);
-
-		// Iterate through each row, printing its cell values.
-		
-		for (ListEntry row : listFeed.getEntries()){
-			if(j == 32){
-				for (String tag : row.getCustomElements().getTags()) {
-					if(i == 2)
-						historyCount = row.getCustomElements().getValue(tag).toString();
-					i++;
-				}
-				i = 1;
-			}
-			if(j == 33){
-				for (String tag : row.getCustomElements().getTags()) {
-					if(i == 2)
-						historyTotal = row.getCustomElements().getValue(tag).toString();
-					i++;
-				}
-				i = 1;
-			}
-			j++;
-		}
-
-	}
-
-	private SpreadsheetService auth() throws AuthenticationException {
-		SpreadsheetService service = 
-				new SpreadsheetService("MySpreadsheetIntegration-v1");
-
-		service.setUserCredentials(USERNAME, PASSWORD);
-
-		return service;
 	}
 
 }
