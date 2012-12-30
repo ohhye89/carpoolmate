d19779ded0ccccb5d7cec1622d092b9f3e2e74cc
Ohye <ohhye89@gmail.com>
2012-12-30 19:47:37 +0900

    7:47

--- src/com/alphastudio/carpoolmate/MainActivity.java
+++ src/com/alphastudio/carpoolmate/MainActivity.java
@@ -1,40 +1,16 @@
 package com.alphastudio.carpoolmate;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Calendar;
-import java.util.List;
-
 import android.app.ActivityGroup;
-import android.app.AlertDialog;
-import android.app.TabActivity;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.ProgressBar;
 import android.widget.TabHost;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.gdata.client.spreadsheet.SpreadsheetService;
-import com.google.gdata.data.spreadsheet.ListEntry;
-import com.google.gdata.data.spreadsheet.ListFeed;
-import com.google.gdata.data.spreadsheet.SpreadsheetEntry;
-import com.google.gdata.data.spreadsheet.SpreadsheetFeed;
-import com.google.gdata.data.spreadsheet.WorksheetEntry;
-import com.google.gdata.data.spreadsheet.WorksheetFeed;
-import com.google.gdata.util.AuthenticationException;
-import com.google.gdata.util.ServiceException;
 
 @SuppressWarnings("deprecation")
 public class MainActivity extends ActivityGroup implements OnClickListener {
 	private TabHost mTabHost;
+	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
